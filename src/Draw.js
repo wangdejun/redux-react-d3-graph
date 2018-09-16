8d03366b5ff923ca0a013d632c492dd75937a556
wangdejun <wangdejun@datalliance.com.cn>
2018-09-17 00:04:18 +0800

    fix

--- src/Draw.js
+++ src/Draw.js
@@ -1,7 +1,34 @@
 const d3 = require('d3');
 
 export default class Draw{
-    init(){
-        const svg = d3.select("#board");
-    }
+  constructor(){
+      this.option = {};
+  }
+
+  drawLink(data){
+    let self = this;
+    let pathNow = d3.select('.container')
+      .append('g')
+      .on('click',function(d){
+        console.log("do something here when a link is clicked");
+      });
+    pathNow.append('path')
+      .datum(data)
+      .attr('d', function(t){
+        return self.createD(
+          {x: t.source.x, y: t.source.y + t.source.r}, 
+          {x: t.target.x, y: t.target.y - t.target.r},
+          10,
+          100
+        );
+      })
+  }
+
+  createD(source, target, dx, dy){
+    return 'M ' + start.x + ' ' + start.y + ' C ' +(start.x - dx) +' ' + (start.y+ dy) + ' ' + (end.x + dx) + ' ' + (end.y - dy) + ' ' + end.x + ' ' + end.y;
+  }
+
+  init(){
+      const svg = d3.select("#board");
+  }
 }
--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ class GraphFlow extends Component {
     return (
       <div>
         <svg id="board" width={2000} height={2000}>
+          <g>
+            <defs>
+              <marker id="arrow" markerUnits="strokeWidth" markerWidth="12" markerHeight="12" viewBox="0 0 12 12" refX="6" refY="6" orient="auto">
+                <path d="M0,3 L6,6 L0,9 L3,6 L0,3" fill="#000"></path>
+              </marker>
+            </defs>
+          </g>
           <g id="main" className="container" />
         </svg>
       </div>
