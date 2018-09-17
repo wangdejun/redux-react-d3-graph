3dd52bffed32a1b2a266d90535dbc97ebbcf2b75
wangdejun <wangdejun@datalliance.com.cn>
2018-09-17 08:15:47 +0800

    fix

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,19 +1,15 @@
 
 import React, { Component } from 'react';
+import GraphFlow from '../../src';
 
 class Test extends Component {
     render() {
         return (
             <div>
-                <GraphFlow
+                <GraphFlow/>
             </div>
         );
     }
 }
 
-export default Test;
-
-ReactDOM.render(
-    <Test>,
-    document.getElementById('root');
-)
+ReactDOM.render(<Test>, document.getElementById('root'));
