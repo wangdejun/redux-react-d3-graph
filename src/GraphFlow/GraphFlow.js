b93d69e9811444dd662be191d28caf0f5a899799
wangdejun <wangdejun@datalliance.com.cn>
2018-10-01 20:34:36 +0800

    rm state data in the graphflow

--- src/GraphFlow/GraphFlow.js
+++ src/GraphFlow/GraphFlow.js
@@ -5,25 +5,6 @@ import './GraphFlow.less';
 class GraphFlow extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      data:{
-        nodes:[
-          {'name':'graph-flow-1','x':500,'y':600, width:50, height:50, bgcolor:"rgba(47, 67, 114, 0.5)"},
-          {'name':'graph-flow-2','x':100,'y':200, width:50, height:50, bgcolor:"rgba(47, 67, 114, 0.5)"},
-          {'name':'graph-flow-3','x':200,'y':400, width:50, height:50, bgcolor:"rgba(47, 67, 114, 0.5)"},
-        ],
-        links:[
-          {
-            'source':{x:500, y: 600, r:50},
-            'target':{x:100, y: 200, r:50},
-          },
-          {
-            'source':{x:200, y: 400, r:50},
-            'target':{x:100, y: 200, r:50},
-          }
-        ],
-      }
-    }
     this.draw = new Draw();
   }
 
